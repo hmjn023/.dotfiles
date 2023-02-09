9b7a50cc473c8ffb43ec17435c51c4052612d6ba
hmjn023 <hmjn023@gmail.com>
2023-02-09 22:40:49 +0900

    add ommisharp and wezterm

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,19 +1,12 @@
--- Mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
   local bufopts = { noremap=true, silent=true, buffer=bufnr }
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
@@ -37,6 +30,20 @@ require'lspconfig'.gopls.setup{}
 require'lspconfig'.clangd.setup{}
 require'lspconfig'.kotlin_language_server.setup{}
 require'lspconfig'.ltex.setup{}
+require'lspconfig'.omnisharp.setup{
+	cmd = {"/home/hmjn/.local/share/nvim/mason/bin/omnisharp","-lsp"},
+	enable_editorconfig_support = true,
+   enable_ms_build_load_projects_on_demand = false,
+   enable_roslyn_analyzers = false,
+   organize_imports_on_format = false,
+   enable_import_completion = false,
+   sdk_include_prereleases = true,
+   analyze_open_documents_only = false,
+	--on_attach = on_attach,
+}
+require'lspconfig'.rust_analyzer.setup{
+	on_attach = on_attach,
+}
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
@@ -46,37 +53,20 @@ require'lspconfig'.sumneko_lua.setup {
   settings = {
     Lua = {
       runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
         version = 'LuaJIT',
       },
       diagnostics = {
-        -- Get the language server to recognize the `vim` global
         globals = {'vim'},
       },
       workspace = {
-        -- Make the server aware of Neovim runtime files
         library = vim.api.nvim_get_runtime_file("", true),
       },
-      -- Do not send telemetry data containing a randomized but unique identifier
       telemetry = {
         enable = false,
       },
     },
   },
 }
-
-
---vim.cmd [[
---set updatetime=500
---highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
---highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
---highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
---augroup lsp_document_highlight
---  autocmd!
---  autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
---  autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
---augroup END
---]]
 
 local cmp = require("cmp")
 cmp.setup({
@@ -87,8 +77,6 @@ cmp.setup({
 	},
 	sources = {
 		{ name = "nvim_lsp" },
-		-- { name = "buffer" },
-		-- { name = "path" },
 	},
 	mapping = cmp.mapping.preset.insert({
 		["<C-p>"] = cmp.mapping.select_prev_item(),
@@ -101,34 +89,20 @@ cmp.setup({
 		ghost_text = true,
 	},
 })
--- cmp.setup.cmdline('/', {
---   mapping = cmp.mapping.preset.cmdline(),
---   sources = {
---     { name = 'buffer' }
---   }
--- })
--- cmp.setup.cmdline(":", {
---   mapping = cmp.mapping.preset.cmdline(),
---   sources = {
---     { name = "path" },
---     { name = "cmdline" },
---   },
--- })
 
+local gid = vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
+vim.api.nvim_create_autocmd("CursorHold" , {
+  group = gid,
+  buffer = bufnr,
+  callback = function ()
+    vim.lsp.buf.document_highlight()
+  end
+})
 
-  local gid = vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
-  vim.api.nvim_create_autocmd("CursorHold" , {
-    group = gid,
-    buffer = bufnr,
-    callback = function ()
-      vim.lsp.buf.document_highlight()
-    end
-  })
-
-  vim.api.nvim_create_autocmd("CursorMoved" , {
-    group = gid,
-    buffer = bufnr,
-    callback = function ()
-      vim.lsp.buf.clear_references()
-    end
-  })
+vim.api.nvim_create_autocmd("CursorMoved" , {
+  group = gid,
+  buffer = bufnr,
+  callback = function ()
+    vim.lsp.buf.clear_references()
+  end
+})
