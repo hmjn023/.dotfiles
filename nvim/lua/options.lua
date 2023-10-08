c2453a715a7c84410f17cb7f7ec147dc8e151672
hmjn023 <hmjn023@gmail.com>
2023-10-08 21:58:26 +0900

    fix nvim

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,41 +1,41 @@
 vim.opt.number = true
 vim.opt.autoindent = true
 vim.opt.tabstop = 3
-vim.opt.shiftwidth =3
-vim.opt.mouse="a"
-vim.opt.splitright= true
+vim.opt.shiftwidth = 3
+vim.opt.mouse = "a"
+vim.opt.splitright = true
 vim.opt.completeopt = "menuone,noinsert"
-vim.opt.pumblend=5
+vim.opt.pumblend = 5
 vim.opt.cursorline = true
 
 require("mason").setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
 })
 require("lsp_signature").setup({
-    bind = true, -- This is mandatory, otherwise border config won't get registered.
-    handler_opts = {
-      border = "rounded"
-    }
- })
+	bind = true, -- This is mandatory, otherwise border config won't get registered.
+	handler_opts = {
+		border = "rounded",
+	},
+})
 
 require("nvim-treesitter.configs").setup({
 	highlight = {
 		enable = true,
-	    disable = {},
+		disable = {},
 	},
 	indent = {
 		enable = true,
-    	disable = {},
+		disable = {},
 	},
 	ensure_installed = {
 		"tsx",
-    	"toml",
+		"toml",
 		"fish",
 		"php",
 		"json",
@@ -43,38 +43,52 @@ require("nvim-treesitter.configs").setup({
 		"swift",
 		"css",
 		"html",
-		"lua"
+		"lua",
+		"bash",
 	},
 	autotag = {
 		enable = true,
 	},
 })
 
-require("telescope").setup {
-  extensions = {
-    media_files = {
-      -- filetypes whitelist
-      -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-      filetypes = {"png", "webp", "jpg", "jpeg"},
-      find_cmd = "rg" -- find command (defaults to `fd`)
-    }
-  },
-}
-require("telescope").load_extension('media_files')
-
+require("telescope").setup({
+	extensions = {
+		media_files = {
+			-- filetypes whitelist
+			-- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+			filetypes = { "png", "webp", "jpg", "jpeg" },
+			find_cmd = "rg", -- find command (defaults to `fd`)
+		},
+	},
+})
+require("telescope").load_extension("media_files")
 
 local sidebar = require("sidebar-nvim")
-local opts = {open = false, initial_width = 20}
+local opts = { open = false, initial_width = 20 }
 sidebar.setup(opts)
 
-require("nvim-autopairs").setup{}
-require("toggleterm").setup{}
+require("nvim-autopairs").setup({})
+require("toggleterm").setup({})
 require("aerial").setup({
 	on_attach = function(bufnr)
-		vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', {buffer = bufnr})
-		vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', {buffer = bufnr})
-	end
+		vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+		vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+	end,
 })
 
-require("lualine").setup()
-require("formatter").setup{}
+require("lualine").setup({})
+
+local format_on_save = require("format-on-save")
+local formatters = require("format-on-save.formatters")
+require("format-on-save").setup({
+	formatter_by_ft = {
+		css = formatters.lsp,
+		html = formatters.lsp,
+		javascript = formatters.lsp,
+		lua = formatters.stylua,
+		rust = formatters.lsp,
+		typescript = formatters.prettierd,
+		typescriptreact = formatters.prettierd,
+		python = formatters.black,
+	},
+})
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,61 +1,70 @@
-vim.cmd[[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-require'packer'.startup(function()
-	use'ryanoasis/vim-devicons'
-	use'tpope/vim-commentary'
-	use'preservim/nerdtree'
-	use'EdenEast/nightfox.nvim'
-	use'neovim/nvim-lspconfig'
-	use'ray-x/lsp_signature.nvim'
-	use'williamboman/mason.nvim'
-	use'williamboman/mason-lspconfig.nvim'
-	use'hrsh7th/nvim-cmp'
-	use'onsails/lspkind.nvim'
-	use'hrsh7th/cmp-nvim-lsp'
-	use'hrsh7th/vim-vsnip'
-	use'nvim-lua/popup.nvim'
-	use'nvim-lua/plenary.nvim'
-	use'nvim-telescope/telescope.nvim' tag = '0.1.0'
-	use'nvim-telescope/telescope-frecency.nvim'
-	use'nvim-telescope/telescope-media-files.nvim'
-	use'GustavoKatel/sidebar.nvim'
-	use'goolord/alpha-nvim'
-	use'terryma/vim-multiple-cursors'
-	use'windwp/nvim-autopairs'
-	use'windwp/nvim-ts-autotag'
-	use'nvim-treesitter/nvim-treesitter'
-	use'mhartington/formatter.nvim'
-	use'nvim-lualine/lualine.nvim' opt=true
-	use {"akinsho/toggleterm.nvim", tag = '*', config = function()
-		require("toggleterm").setup()
-	end}
+require("packer").startup(function()
+	use("wbthomason/packer.nvim")
+	use("ryanoasis/vim-devicons")
+	use("tpope/vim-commentary")
+	use("preservim/nerdtree")
+	use("EdenEast/nightfox.nvim")
+	use("neovim/nvim-lspconfig")
+	use("ray-x/lsp_signature.nvim")
+	use("williamboman/mason.nvim")
+	use("williamboman/mason-lspconfig.nvim")
+	use("hrsh7th/nvim-cmp")
+	use("onsails/lspkind.nvim")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/vim-vsnip")
+	use("nvim-lua/popup.nvim")
+	use("nvim-lua/plenary.nvim")
+	use("nvim-telescope/telescope.nvim")
+	tag = "0.1.0"
+	use("nvim-telescope/telescope-frecency.nvim")
+	use("nvim-telescope/telescope-media-files.nvim")
+	use("GustavoKatel/sidebar.nvim")
+	use("goolord/alpha-nvim")
+	use("terryma/vim-multiple-cursors")
+	use("windwp/nvim-autopairs")
+	use("windwp/nvim-ts-autotag")
+	use("nvim-treesitter/nvim-treesitter")
+	use("elentok/format-on-save.nvim")
+	use("nvim-lualine/lualine.nvim")
+	opt = true
+	use({
+		"akinsho/toggleterm.nvim",
+		tag = "*",
+		config = function()
+			require("toggleterm").setup()
+		end,
+	})
 
 	vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
-	use{
+	use({
 		"nvim-neo-tree/neo-tree.nvim",
-	    branch = "v2.x",
-	    requires = {
-      		"nvim-lua/plenary.nvim",
-      		"kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
-      		"MunifTanjim/nui.nvim",
-		}
-	}
-	use{
-		'andymass/vim-matchup',
+		branch = "v2.x",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		},
+	})
+	use({
+		"andymass/vim-matchup",
 		setup = function()
-      vim.g.matchup_matchparen_offscreen = { method = "popup" }
-	end
-	}
-	use{
-		'stevearc/aerial.nvim',
-		config = function() require('aerial').setup() end
-	}
+			vim.g.matchup_matchparen_offscreen = { method = "popup" }
+		end,
+	})
+	use({
+		"stevearc/aerial.nvim",
+		config = function()
+			require("aerial").setup()
+		end,
+	})
 	use({
 		"iamcco/markdown-preview.nvim",
-		run = function() vim.fn["mkdp#util#install"]() end,
+		run = function()
+			vim.fn["mkdp#util#install"]()
+		end,
 	})
 end)
-
-
 
 vim.cmd("colorscheme nightfox")
