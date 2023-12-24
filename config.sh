69ff3b46b87b228d009cc9c28d198bcd40d4fef1
hmjn023 <hmjn023@gmail.com>
2023-12-24 22:52:59 +0900

    pacman

--- config.sh
+++ config.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 ln -sf $HOME/.dotfiles/sway $HOME/.config
+ln -sf $HOME/.dotfiles/hypr $HOME/.config
 ln -sf $HOME/.dotfiles/kitty $HOME/.config
 ln -sf $HOME/.dotfiles/ranger $HOME/.config
 ln -sf $HOME/.dotfiles/nvim $HOME/.config
+ln -sf $HOME/.dotfiles/wezterm.lua $HOME/.wezterm.lua
+ln -sf $HOME/.dotfiles/tmux.conf $HOME/.tmux.cong
